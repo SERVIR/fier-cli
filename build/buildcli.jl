c646a5d50589773f77a1eb1eb564bf4993a29383
kmarkert <kel.markert@gmail.com>
2022-01-26 11:10:52 -0700

    adding check for packagecompiler and install if not available

--- build/buildcli.jl
+++ build/buildcli.jl
@@ -1,3 +1,11 @@
-using PackageCompiler
+using Logging
+try
+    using PackageCompiler
+catch
+    @warn "PackageCompiler is not installed, attempting to install"
+    using Pkg
+    Pkg.add("PackageCompiler")
+    using PackageCompiler
+end
 
 create_app("..", "fierapp", filter_stdlibs = true)
